dcc6ef54789b167b80891dab45f47c5949bfb4f4
qiaolei <a346750236@vip.qq.com>
2020-01-21 10:05:39 +0800

    Element和axios还有一些资源准备完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '../src/styles/index.less'
+// 引入axios
+import axios from 'axios'
+Vue.prototype.$axios = axios // 赋值给全局对象
+Vue.use(ElementUI) //  引入样式
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {},
+  props: {},
+  data () {
+    return {
+      message: ''
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+</style>
