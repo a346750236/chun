80e2365e8f42bdb36921223754b121e20dcc6078
qiaolei <a346750236@vip.qq.com>
2020-01-24 11:59:15 +0800

    评论分页组件完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -15,10 +15,25 @@
       <el-table-column label="操作">
         <template slot-scope="object">
           <el-button type="text" size="small">修改</el-button>
-          <el-button type="text" size="small" @click="Opcomment(object.row)">{{ object.row.comment_status ? '关闭评论' : '打开评论' }}</el-button>
+          <el-button
+            type="text"
+            size="small"
+            @click="Opcomment(object.row)"
+          >{{ object.row.comment_status ? '关闭评论' : '打开评论' }}</el-button>
         </template>
-</el-table-column>
+      </el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <el-row type="flex" justify="center" style="height:80px" align="middle">
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="page.totle"
+      :page-size="page.pageSize"
+      :currentPage="page.currentPage"
+      @current-change="changePage"
+    ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -29,7 +44,13 @@ export default {
   props: {},
   data () {
     return {
-      list: [] // 评论数据
+      list: [], // 评论数据
+      page: {
+        // 分页数据
+        totle: 0, // 数据总条数
+        pageSize: 10, // 默认每页10条
+        currentPage: 1 //  当前页码 默认显示第一页
+      }
     }
   },
   computed: {},
@@ -40,16 +61,25 @@ export default {
   },
   mounted () {},
   methods: {
+    // 切换分页
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      // 重新加载页面
+      this.Getcomment()
+    },
     // 请求评论列表数据
     async Getcomment () {
       const result = await this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
         }
       })
       // console.log(result.data)
       this.list = result.data.results
+      this.page.totle = result.data.total_count
     },
     // 评论状态
     CommentState (row, column, cellValue, index) {
