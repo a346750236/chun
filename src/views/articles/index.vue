6fa28245af8f54524a2eb9eac9c705aa2f238177
qiaolei <a346750236@vip.qq.com>
2020-01-29 10:13:49 +0800

    内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,11 +1,63 @@
 <template>
   <!-- 卡片 -->
-   <el-card>
-     <!-- 面包屑 -->
-     <my-bread slot="header">
-        <template slot="title">文章列表</template>
-     </my-bread>
-   </el-card>
+  <el-card class="searchCard">
+    <!-- 面包屑 -->
+    <my-bread slot="header">
+      <template slot="title">文章列表</template>
+    </my-bread>
+    <!-- 行 -->
+    <el-row class="searchTool">
+      <!-- 列 -->
+      <el-col :span="2">
+        <span>文章状态</span>
+      </el-col>
+      <el-col :span="18">
+        <!--
+              0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部
+        -->
+        <!-- 单选框组 -->
+        <el-radio-group>
+          <el-radio>全部</el-radio>
+          <el-radio>草稿</el-radio>
+          <el-radio>待审核</el-radio>
+          <el-radio>审核通过</el-radio>
+          <el-radio>审核失败</el-radio>
+        </el-radio-group>
+      </el-col>
+    </el-row>
+    <!-- 行 -->
+    <el-row class="searchTool">
+      <!-- 列 -->
+      <el-col :span="2">
+        <span>频道列表</span>
+      </el-col>
+      <el-col :span="18">
+          <!-- 循环生成多个el-option
+              label 指的是 el-option显示值
+              value指的是 el-option的存储值
+            -->
+        <el-select  placeholder="请选择">
+          <el-option></el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <!-- 行 -->
+    <el-row class="searchTool">
+      <!-- 列 -->
+      <el-col :span="2">
+        <span>时间选择</span>
+      </el-col>
+      <el-col :span="18">
+        <!-- 时间选择器 -->
+         <el-date-picker
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
@@ -27,5 +79,10 @@ export default {
 </script>
 
 <style scoped lang="less">
-
+.searchCard {
+  .searchTool {
+    padding: 20px 0;
+    margin-left: 30px;
+  }
+}
 </style>
