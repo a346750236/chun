ec438bf86a110baa80a04375fee3410257104290
qiaolei <a346750236@vip.qq.com>
2020-01-29 10:41:36 +0800

    内容列表上面部分完成

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -16,12 +16,13 @@
               0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部
         -->
         <!-- 单选框组 -->
-        <el-radio-group>
-          <el-radio>全部</el-radio>
-          <el-radio>草稿</el-radio>
-          <el-radio>待审核</el-radio>
-          <el-radio>审核通过</el-radio>
-          <el-radio>审核失败</el-radio>
+        <el-radio-group v-model="FormData.status">
+          <!-- 全部为5  判断是不是全部 如果是就传入null -->
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
         </el-radio-group>
       </el-col>
     </el-row>
@@ -36,8 +37,13 @@
               label 指的是 el-option显示值
               value指的是 el-option的存储值
             -->
-        <el-select  placeholder="请选择">
-          <el-option></el-option>
+        <el-select v-model="FormData.channel_id">
+          <el-option
+          v-for="item in channel"
+           :key="item.id"
+            :value="item.id"
+            :label="item.name">
+            </el-option>
         </el-select>
       </el-col>
     </el-row>
@@ -50,6 +56,7 @@
       <el-col :span="18">
         <!-- 时间选择器 -->
          <el-date-picker
+          v-model="FormData.dateRange"
           type="daterange"
           range-separator="-"
           start-placeholder="开始日期"
@@ -67,14 +74,30 @@ export default {
   props: {},
   data () {
     return {
-      message: ''
+      channel: [], // 频道列表
+      FormData: {
+        status: 5, // 默认为全部
+        channel_id: null, // 默认为null
+        dateRange: [] // 时间是个数组
+      }
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    // 频道列表
+    this.Getchannel()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    // 获取频道
+    async Getchannel () {
+      const result = await this.$axios({
+        url: '/channels'
+      })
+      this.channel = result.data.channels
+    }
+  }
 }
 </script>
 
