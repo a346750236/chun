6d5294fec0f79a730f8e38f174fe0578ba3826f0
qiaolei <a346750236@vip.qq.com>
2020-01-30 11:30:36 +0800

    发表文章-发布文章检验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -6,32 +6,32 @@
       <template slot="title">发布文章</template>
     </my-bread>
     <!-- 页面结构 -->
-    <el-form style="margin-left:50px">
-      <el-form-item label="标题：">
-        <el-input style="width:50%"></el-input>
+    <el-form ref="myForm" :model="formData" :rules="rules" style="margin-left:50px">
+      <el-form-item prop="title" label="标题：">
+        <el-input v-model="formData.title" style="width:50%"></el-input>
       </el-form-item>
-      <el-form-item label="内容：">
+      <el-form-item prop="content"  label="内容：">
         <!-- 文本框 -->
-        <el-input type="textarea" :rows="4" style="width:80%"></el-input>
+        <el-input v-model="formData.content" type="textarea" :rows="4" style="width:80%"></el-input>
       </el-form-item>
-      <el-form-item label="封面：">
+      <el-form-item label="封面：" prop="type">
         <!-- 单选组 -->
-        <el-radio-group>
-          <el-radio>单图</el-radio>
-          <el-radio>三图</el-radio>
-          <el-radio>无图</el-radio>
-          <el-radio>自动</el-radio>
+        <el-radio-group v-model="formData.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
       <!-- 下拉框 -->
-      <el-form-item label="频道：">
-        <el-select>
+      <el-form-item label="频道：" prop="channel_id">
+        <el-select v-model="formData.channel_id">
           <el-option v-for="item in channel" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">发布</el-button>
-        <el-button>存入草稿</el-button>
+        <el-button @click="publishArticle" type="primary">发布</el-button>
+        <el-button @click="publishArticle">存入草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -44,7 +44,26 @@ export default {
   data () {
     return {
       // 频道列表
-      channel: []
+      channel: [],
+      // 校验数据
+      formData: {
+        title: '', // 标题
+        content: '', // 内容
+        cover: {
+          type: 0, //   封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: [] // 存储的图片的地址
+        },
+        channel_id: null // 频道id
+      },
+      // 校验
+      rules: {
+        // 校验规则对象 min  max
+        title: [{ required: true, message: '标题内容不能为空' }, {
+          min: 5, max: 30, message: '标题长度需要在5到30字符之间'
+        }],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '频道分类不能为空' }]
+      }
     }
   },
   computed: {},
@@ -55,6 +74,13 @@ export default {
   },
   mounted () {},
   methods: {
+    publishArticle () {
+      this.$refs.myForm.validate(isok => {
+        if (isok) {
+          console.log('校验成功')
+        }
+      })
+    },
     // 获取频道
     async Getchannel () {
       const result = await this.$axios({
