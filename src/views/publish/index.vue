2f742345bbf164939b054fdadd9926b9a54d8942
qiaolei <a346750236@vip.qq.com>
2020-01-30 10:54:46 +0800

    发布文章新建页面-页面结构-频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,25 +1,70 @@
 <template>
-  <div class="post-comment">评论项</div>
+  <!-- 卡片 -->
+  <el-card>
+    <!-- 面包屑 -->
+    <my-bread slot="header">
+      <template slot="title">发布文章</template>
+    </my-bread>
+    <!-- 页面结构 -->
+    <el-form style="margin-left:50px">
+      <el-form-item label="标题：">
+        <el-input style="width:50%"></el-input>
+      </el-form-item>
+      <el-form-item label="内容：">
+        <!-- 文本框 -->
+        <el-input type="textarea" :rows="4" style="width:80%"></el-input>
+      </el-form-item>
+      <el-form-item label="封面：">
+        <!-- 单选组 -->
+        <el-radio-group>
+          <el-radio>单图</el-radio>
+          <el-radio>三图</el-radio>
+          <el-radio>无图</el-radio>
+          <el-radio>自动</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <!-- 下拉框 -->
+      <el-form-item label="频道：">
+        <el-select>
+          <el-option v-for="item in channel" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发布</el-button>
+        <el-button>存入草稿</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
 export default {
-  name: 'PostComment',
   components: {},
   props: {},
   data () {
     return {
-      message: ''
+      // 频道列表
+      channel: []
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    // 获取频道
+    this.Getchannel()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    // 获取频道
+    async Getchannel () {
+      const result = await this.$axios({
+        url: '/channels'
+      })
+      this.channel = result.data.channels
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
-
 </style>
