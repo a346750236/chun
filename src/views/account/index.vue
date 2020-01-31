2fb91c5b5d46a882d8dc9f1647d1238655b2c603
qiaolei <a346750236@vip.qq.com>
2020-01-31 13:52:19 +0800

    账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,21 +5,21 @@
     </my-bread>
     <!-- 放置上传组件 -->
       <el-upload class='head-upload' action="ql" :show-file-list="false">
-          <img src="../../assets/img/admire.png" alt="">
+          <img :src="formData.photo ? formData.photo : defaultImg" alt="">
       </el-upload>
     <!-- 放置Form组件 -->
     <el-form style="margin-left:60px" label-width="100px">
       <el-form-item label="用户名">
-         <el-input style="width:30%"></el-input>
+         <el-input v-model="formData.name" style="width:30%"></el-input>
       </el-form-item>
       <el-form-item label="简介">
-         <el-input style="width:30%"></el-input>
+         <el-input v-model="formData.intro" style="width:30%"></el-input>
       </el-form-item>
       <el-form-item label="邮箱">
-         <el-input style="width:30%"></el-input>
+         <el-input v-model="formData.email" style="width:30%"></el-input>
       </el-form-item>
       <el-form-item label="手机号">
-         <el-input style="width:30%" disabled></el-input>
+         <el-input v-model="formData.mobile" style="width:30%" disabled></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">保存信息</el-button>
@@ -35,14 +35,33 @@ export default {
   props: {},
   data () {
     return {
-      message: ''
+      // 定义一个表单数据对象
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
+      },
+      defaultImg: require('../../assets/img/admire.png')
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.getUserInfo()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    // 获取用户信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  }
 }
 </script>
 
