1531710f2bd26d23d92b86d9e822f820a91762ec
qiaolei <a346750236@vip.qq.com>
2020-02-02 19:26:23 +0800

    页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,8 +1,12 @@
 // 权限拦截 导航守卫
 import router from '../router/index.js'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 // 路由守卫设置
 // 全局前置守卫
 router.beforeEach(function (to, from, next) {
+  progress.start() // 开启进度条
   // 判断 拦截的范围
   if (to.path.startsWith('/home')) {
     // 进入到了拦截范围
@@ -17,4 +21,8 @@ router.beforeEach(function (to, from, next) {
   } else {
     next() // 放行
   }
+})
+// afterEach
+router.afterEach(function () {
+  progress.done() // 关闭进度条
 })
